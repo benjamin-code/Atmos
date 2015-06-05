cf26342dbace6971b4936199c6a5518aaffde68c
Ryan Gehrig <ryan.d.gehrig@hp.com>
2015-06-05 11:30:36 -0700

    Removing broken dir structure

--- roles/pon/rws-build.rb
+++
@@ -1,3 +0,0 @@
-name "rws-build"
-description "Setup a a build server with Maven etc to build RWS"
-run_list "recipe[rws]","recipe[rws-build]"
--- roles/pon/vlantool-server.rb
+++
@@ -1,3 +0,0 @@
-name "vlantool-server"
-description "For vlantool servers"
-run_list "recipe[vlantool]","recipe[api4vlan]","recipe[api4vlan-receiver-dist]","recipe[api4vlan-netconf]","recipe[api4vlan-web]"
